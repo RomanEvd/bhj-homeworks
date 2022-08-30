2d833f405bc7195e31af10df14dad7c9827a3391
RomanEvd <evdokimov.roman.1993@mail.ru>
2022-08-30 21:14:44 +0400

    auotocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,17 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    const arrayOfContacts = [];
+    for (let option of this.input.options) {
+      if (option.innerText.includes(text)) {
+        arrayOfContacts.push({text: option.innerText, value: option.value});
       }
-    */
-    return [
-      {
+    }
+    return arrayOfContacts;
+      /*{
         text: 'Чубакка',
         value: '1'
-      }
-    ];
+      }*/
   }
 }
 
