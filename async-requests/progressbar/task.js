6aa264d8a9bbab39c18d4f406c09339a1d9381fc
RomanEvd <evdokimov.roman.1993@mail.ru>
2022-09-04 16:38:11 +0400

    async-requests

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,17 @@
+const formEl = document.getElementById('form');
+const progressEl = document.getElementById('progress');
+
+formEl.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const request = new XMLHttpRequest();
+    request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    
+    const file = document.getElementById('file').files;
+    const fileName = document.querySelector('.input__wrapper-desc');
+    const formData = new FormData();
+    formData.append(`${fileName.innerText}`, file[0]);
+    request.upload.addEventListener('progress', function (ev){
+        progressEl.value = (ev.loaded / ev.total).toFixed(2);
+    }, false);
+    request.send(formData);
+});
